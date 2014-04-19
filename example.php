fdbac586ecb65764c538b81bb19402707161aae4
Nicholas Rejack <nrejack@ufl.edu>
2014-04-19 01:13:07 -0400

    Initial commit

---
+++ example.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>example</title>
+	</head>
+
+	<body>
+			<section>
+<?php
+echo "Hello World!";
+
+
+
+	echo "Hello World!";
+
+
+?>
+			</section>
+	</body>
+
+</html>
---
+++ file.php
@@ -0,0 +1,6 @@
+<?php
+	print('Your name is: '.$_GET['myname']);
+	print('<br>');
+	print('Your value is: '.$_GET['myvalue']);
+	print('Your value is: '.$_GET['mysearch']);
+	?>
